dacde0bef373736f05c318fe62ffa67330da0535
admin_0 <admin_0@WIN-R1LE9N2JJ8S>
2023-10-30 14:27:15 +0300

    Добавьте файлы проекта.

---
+++ Shef/Form1.Designer.cs
@@ -0,0 +1,283 @@
+﻿namespace Shef
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.comboBox3 = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.labelRez1 = new System.Windows.Forms.Label();
+            this.labelRez2 = new System.Windows.Forms.Label();
+            this.labelRez3 = new System.Windows.Forms.Label();
+            this.comboBox4 = new System.Windows.Forms.ComboBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.labelRez4 = new System.Windows.Forms.Label();
+            this.but1 = new System.Windows.Forms.Button();
+            this.but2 = new System.Windows.Forms.Button();
+            this.but3 = new System.Windows.Forms.Button();
+            this.but4 = new System.Windows.Forms.Button();
+            this.label11 = new System.Windows.Forms.Label();
+            this.but_reset = new System.Windows.Forms.Button();
+            this.lablePrice = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(137, 31);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 24);
+            this.comboBox1.TabIndex = 0;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Location = new System.Drawing.Point(137, 81);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(121, 24);
+            this.comboBox2.TabIndex = 1;
+            this.comboBox2.SelectedIndexChanged += new System.EventHandler(this.comboBox2_SelectedIndexChanged);
+            // 
+            // comboBox3
+            // 
+            this.comboBox3.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox3.FormattingEnabled = true;
+            this.comboBox3.Location = new System.Drawing.Point(137, 132);
+            this.comboBox3.Name = "comboBox3";
+            this.comboBox3.Size = new System.Drawing.Size(121, 24);
+            this.comboBox3.TabIndex = 2;
+            this.comboBox3.SelectedIndexChanged += new System.EventHandler(this.comboBox3_SelectedIndexChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(28, 34);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(50, 16);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Блюдо";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(28, 84);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(54, 16);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Гарнир";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(28, 135);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(63, 16);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Напитки";
+            // 
+            // labelRez1
+            // 
+            this.labelRez1.AutoSize = true;
+            this.labelRez1.Location = new System.Drawing.Point(306, 34);
+            this.labelRez1.Name = "labelRez1";
+            this.labelRez1.Size = new System.Drawing.Size(0, 16);
+            this.labelRez1.TabIndex = 6;
+            // 
+            // labelRez2
+            // 
+            this.labelRez2.AutoSize = true;
+            this.labelRez2.Location = new System.Drawing.Point(306, 84);
+            this.labelRez2.Name = "labelRez2";
+            this.labelRez2.Size = new System.Drawing.Size(0, 16);
+            this.labelRez2.TabIndex = 7;
+            // 
+            // labelRez3
+            // 
+            this.labelRez3.AutoSize = true;
+            this.labelRez3.Location = new System.Drawing.Point(306, 135);
+            this.labelRez3.Name = "labelRez3";
+            this.labelRez3.Size = new System.Drawing.Size(0, 16);
+            this.labelRez3.TabIndex = 8;
+            // 
+            // comboBox4
+            // 
+            this.comboBox4.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox4.FormattingEnabled = true;
+            this.comboBox4.Location = new System.Drawing.Point(137, 183);
+            this.comboBox4.Name = "comboBox4";
+            this.comboBox4.Size = new System.Drawing.Size(121, 24);
+            this.comboBox4.TabIndex = 9;
+            this.comboBox4.SelectedIndexChanged += new System.EventHandler(this.comboBox4_SelectedIndexChanged);
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(28, 186);
+            this.label7.Name = "label7";
+            this.label7.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.label7.Size = new System.Drawing.Size(61, 16);
+            this.label7.TabIndex = 11;
+            this.label7.Text = "Закуски";
+            // 
+            // labelRez4
+            // 
+            this.labelRez4.AutoSize = true;
+            this.labelRez4.Location = new System.Drawing.Point(306, 186);
+            this.labelRez4.Name = "labelRez4";
+            this.labelRez4.Size = new System.Drawing.Size(0, 16);
+            this.labelRez4.TabIndex = 13;
+            // 
+            // but1
+            // 
+            this.but1.Location = new System.Drawing.Point(427, 31);
+            this.but1.Name = "but1";
+            this.but1.Size = new System.Drawing.Size(83, 23);
+            this.but1.TabIndex = 15;
+            this.but1.Text = "Добавить";
+            this.but1.UseVisualStyleBackColor = true;
+            this.but1.Click += new System.EventHandler(this.but1_Click);
+            // 
+            // but2
+            // 
+            this.but2.Location = new System.Drawing.Point(427, 81);
+            this.but2.Name = "but2";
+            this.but2.Size = new System.Drawing.Size(83, 23);
+            this.but2.TabIndex = 16;
+            this.but2.Text = "Добавить";
+            this.but2.UseVisualStyleBackColor = true;
+            this.but2.Click += new System.EventHandler(this.but2_Click);
+            // 
+            // but3
+            // 
+            this.but3.Location = new System.Drawing.Point(427, 128);
+            this.but3.Name = "but3";
+            this.but3.Size = new System.Drawing.Size(83, 23);
+            this.but3.TabIndex = 17;
+            this.but3.Text = "Добавить";
+            this.but3.UseVisualStyleBackColor = true;
+            this.but3.Click += new System.EventHandler(this.but3_Click);
+            // 
+            // but4
+            // 
+            this.but4.Location = new System.Drawing.Point(427, 179);
+            this.but4.Name = "but4";
+            this.but4.Size = new System.Drawing.Size(83, 23);
+            this.but4.TabIndex = 18;
+            this.but4.Text = "Добавить";
+            this.but4.UseVisualStyleBackColor = true;
+            this.but4.Click += new System.EventHandler(this.but4_Click);
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Location = new System.Drawing.Point(31, 278);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(43, 16);
+            this.label11.TabIndex = 20;
+            this.label11.Text = "Цена:";
+            // 
+            // but_reset
+            // 
+            this.but_reset.Location = new System.Drawing.Point(34, 320);
+            this.but_reset.Name = "but_reset";
+            this.but_reset.Size = new System.Drawing.Size(75, 23);
+            this.but_reset.TabIndex = 21;
+            this.but_reset.Text = "Сброс";
+            this.but_reset.UseVisualStyleBackColor = true;
+            this.but_reset.Click += new System.EventHandler(this.butreset_Click);
+            // 
+            // lablePrice
+            // 
+            this.lablePrice.AutoSize = true;
+            this.lablePrice.Location = new System.Drawing.Point(137, 278);
+            this.lablePrice.Name = "lablePrice";
+            this.lablePrice.Size = new System.Drawing.Size(14, 16);
+            this.lablePrice.TabIndex = 22;
+            this.lablePrice.Text = "0";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(541, 368);
+            this.Controls.Add(this.lablePrice);
+            this.Controls.Add(this.but_reset);
+            this.Controls.Add(this.label11);
+            this.Controls.Add(this.but4);
+            this.Controls.Add(this.but3);
+            this.Controls.Add(this.but2);
+            this.Controls.Add(this.but1);
+            this.Controls.Add(this.labelRez4);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.comboBox4);
+            this.Controls.Add(this.labelRez3);
+            this.Controls.Add(this.labelRez2);
+            this.Controls.Add(this.labelRez1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.comboBox3);
+            this.Controls.Add(this.comboBox2);
+            this.Controls.Add(this.comboBox1);
+            this.Name = "Form1";
+            this.Text = "Shef";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.ComboBox comboBox3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label labelRez1;
+        private System.Windows.Forms.Label labelRez2;
+        private System.Windows.Forms.Label labelRez3;
+        private System.Windows.Forms.ComboBox comboBox4;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label labelRez4;
+        private System.Windows.Forms.Button but1;
+        private System.Windows.Forms.Button but2;
+        private System.Windows.Forms.Button but3;
+        private System.Windows.Forms.Button but4;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.Button but_reset;
+        private System.Windows.Forms.Label lablePrice;
+    }
+}
+
---
+++ Shef/Form1.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace Shef
+{
+    public partial class Form1 : Form
+    {
+        public string[] strings1 = new string[] { "Макароны", "Стейк" };
+        public string[] strings2 = new string[] { "Котлета", "Рыба" };
+        public string[] strings3 = new string[] { "Чай", "Кофе" };
+        public string[] strings4 = new string[] { "Оливье", "Цезарь" };
+
+        public int[] ints1 = new int[] {100,1000};
+        public int[] ints2 = new int[] {200,2000};
+        public int[] ints3 = new int[] {300,3000 };
+        public int[] ints4 = new int[] {400,4000 };
+
+
+        public Form1()
+        {
+            InitializeComponent();
+            foreach (var item in strings1 ) 
+            {
+                comboBox1.Items.Add(item);
+            }
+            foreach (var item in strings2)
+            {
+                comboBox2.Items.Add(item);
+            }
+            foreach (var item in strings3)
+            {
+                comboBox3.Items.Add(item);
+            }
+            foreach (var item in strings4)
+            {
+                comboBox4.Items.Add(item);
+            }
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            System.Windows.Forms.ComboBox comboBox = (System.Windows.Forms.ComboBox)sender;
+            string selectedEmployee = (string)comboBox1.SelectedItem;
+            int resultIndex = -1;
+            resultIndex = comboBox1.FindStringExact(selectedEmployee);
+            if (resultIndex != -1)
+            {
+                labelRez1.Text = ints1[resultIndex].ToString();
+            }
+        }
+
+        private void comboBox2_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            System.Windows.Forms.ComboBox comboBox = (System.Windows.Forms.ComboBox)sender;
+            string selectedEmployee = (string)comboBox2.SelectedItem;
+            int resultIndex = -1;
+            resultIndex = comboBox2.FindStringExact(selectedEmployee);
+            if (resultIndex != -1)
+            {
+                labelRez2.Text = ints2[resultIndex].ToString();
+            }
+        }
+
+        private void comboBox3_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            System.Windows.Forms.ComboBox comboBox = (System.Windows.Forms.ComboBox)sender;
+            string selectedEmployee = (string)comboBox3.SelectedItem;
+            int resultIndex = -1;
+            resultIndex = comboBox3.FindStringExact(selectedEmployee);
+            if (resultIndex != -1)
+            {
+                labelRez3.Text = ints3[resultIndex].ToString();
+            }
+        }
+
+        private void comboBox4_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            System.Windows.Forms.ComboBox comboBox = (System.Windows.Forms.ComboBox)sender;
+            string selectedEmployee = (string)comboBox4.SelectedItem;
+            int resultIndex = -1;
+            resultIndex = comboBox4.FindStringExact(selectedEmployee);
+            if (resultIndex != -1)
+            {
+                labelRez4.Text = ints4[resultIndex].ToString();
+            }
+        }
+
+        private void but1_Click(object sender, EventArgs e)
+        {
+            lablePrice.Text = "" + (int.Parse(lablePrice.Text) + int.Parse(labelRez1.Text));
+        }
+
+        private void but2_Click(object sender, EventArgs e)
+        {
+            lablePrice.Text = "" + (int.Parse(lablePrice.Text) + int.Parse(labelRez2.Text));
+        }
+
+        private void but3_Click(object sender, EventArgs e)
+        {
+            lablePrice.Text = "" + (int.Parse(lablePrice.Text) + int.Parse(labelRez3.Text));
+        }
+
+        private void but4_Click(object sender, EventArgs e)
+        {
+            lablePrice.Text = "" + (int.Parse(lablePrice.Text) + int.Parse(labelRez4.Text));
+        }
+
+        private void butreset_Click(object sender, EventArgs e)
+        {
+            lablePrice.Text = "0";
+        }
+    }
+}
